ee249af4e770b1a13f166654c4d4a9b067accdfc
NetStyx <NetStyx@users.noreply.github.com>
2026-06-06 12:56:14 +0200

    resolved compiler messages

--- ARealmRepopulated/Core/Json/JsonExtensions.cs
+++ ARealmRepopulated/Core/Json/JsonExtensions.cs
@@ -5,9 +5,8 @@ namespace ARealmRepopulated.Core.Json;
 public static class JsonExtensions {
 
     public static int GetInt(this JsonNode node) {
-        if (node == null) {
-            throw new ArgumentNullException(nameof(node));
-        }
+        ArgumentNullException.ThrowIfNull(node);
+
         if (node is not JsonValue jsonValue) {
             throw new ArgumentException("Node must be a JSON value", nameof(node));
         }
@@ -21,9 +20,8 @@ public static class JsonExtensions {
     }
 
     public static string GetString(this JsonNode node) {
-        if (node == null) {
-            throw new ArgumentNullException(nameof(node));
-        }
+        ArgumentNullException.ThrowIfNull(node);
+
         if (node is not JsonValue jsonValue) {
             throw new ArgumentException("Node must be a JSON value", nameof(node));
         }
@@ -37,9 +35,8 @@ public static class JsonExtensions {
     }
 
     public static byte GetByte(this JsonNode node) {
-        if (node == null) {
-            throw new ArgumentNullException(nameof(node));
-        }
+        ArgumentNullException.ThrowIfNull(node);
+
         if (node is not JsonValue jsonValue) {
             throw new ArgumentException("Node must be a JSON value", nameof(node));
         }
@@ -53,22 +50,17 @@ public static class JsonExtensions {
     }
 
     public static T GetEnum<T>(this JsonNode node) where T : struct, Enum {
-        if (node == null) {
-            throw new ArgumentNullException(nameof(node));
-        }
+        ArgumentNullException.ThrowIfNull(node);
 
         if (node is not JsonValue jsonValue) {
             throw new ArgumentException("Node must be a JSON value", nameof(node));
         }
 
-        // Handle string values
         if (jsonValue.TryGetValue<string>(out var stringValue)) {
-            // First try parsing by enum name
             if (Enum.TryParse<T>(stringValue, ignoreCase: true, out var result)) {
                 return result;
             }
 
-            // Then try parsing as numeric value
             if (int.TryParse(stringValue, out var numericValue)) {
                 return (T)Enum.ToObject(typeof(T), numericValue);
             }
@@ -76,17 +68,14 @@ public static class JsonExtensions {
             throw new ArgumentException($"Unable to parse '{stringValue}' as enum {typeof(T).Name}");
         }
 
-        // Handle integer values
         if (jsonValue.TryGetValue<int>(out var intValue)) {
             return (T)Enum.ToObject(typeof(T), intValue);
         }
 
-        // Handle long values
         if (jsonValue.TryGetValue<long>(out var longValue)) {
             return (T)Enum.ToObject(typeof(T), longValue);
         }
 
-        // Handle byte values
         if (jsonValue.TryGetValue<byte>(out var byteValue)) {
             return (T)Enum.ToObject(typeof(T), byteValue);
         }
