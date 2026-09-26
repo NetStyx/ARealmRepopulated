1d58c5242208db8d59dfaddb2930cbe9ecc32c06
NetStyx <NetStyx@users.noreply.github.com>
2026-09-26 19:59:33 +0200

    fix(chatcommands): update command handling for /arrp reload

--- ARealmRepopulated/Core/Services/ChatCommands.cs
+++ ARealmRepopulated/Core/Services/ChatCommands.cs
@@ -5,25 +5,26 @@ using Dalamud.Plugin.Services;
 namespace ARealmRepopulated.Core.Services;
 
 public class ChatCommands(Plugin plugin, ICommandManager commandManager, ScenarioOrchestrator scenarioManager) : IDisposable {
+    
+    private const string ArrpCommand = "/arrp";
+    private const string ReloadArgument = "reload";
 
-    private const string OpenArrpCommand = "/arrp";
-    private const string ReloadScenarioCommand = "/arrp reload";
     public void Initialize() {
-        commandManager.AddHandler(OpenArrpCommand, new CommandInfo(OpenConfigWindow) { ShowInHelp = true, HelpMessage = "Opens the configuration window" });
-        commandManager.AddHandler(ReloadScenarioCommand, new CommandInfo(ReloadScenarios) { ShowInHelp = true, HelpMessage = "Reloads the scenarios in the current area" });
+        commandManager.AddHandler(ArrpCommand, new CommandInfo(HandleArrpCommand) {
+            ShowInHelp = true, HelpMessage = $"Opens the configuration window\n{ArrpCommand} {ReloadArgument} → Reloads the scenarios in the current area"
+        });
     }
 
-    private void ReloadScenarios(string _, string __) {
-        scenarioManager.Reload();
-    }
-
-    private void OpenConfigWindow(string command, string args) {
-        plugin.ToggleConfigUI();
+    private void HandleArrpCommand(string command, string args) {
+        if (args.Trim().Equals(ReloadArgument, StringComparison.OrdinalIgnoreCase)) {
+            scenarioManager.Reload();
+        } else {
+            plugin.ToggleConfigUI();
+        }
     }
 
     public void Dispose() {
-        commandManager.RemoveHandler(OpenArrpCommand);
-        commandManager.RemoveHandler(ReloadScenarioCommand);
+        commandManager.RemoveHandler(ArrpCommand);
         GC.SuppressFinalize(this);
     }
 }
